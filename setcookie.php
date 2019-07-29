0c58dfe934c528f8e4136ee93767bd8f3757f115
m-szwarc <marcinszwarc@hotmail.com>
2019-07-29 17:58:39 +0200

    CORS

--- setcookie.php
+++ setcookie.php
@@ -1,10 +1,16 @@
 <?php
+if($_SERVER['REQUEST_METHOD'] == 'OPTIONS'){
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Methods: GET');
+    header('Access-Control-Allow-Credentials: true');
+}
+
 // GET: is_on=true => vector-dark_enable = 1 [DEFAULT]
 // GET: is_on=false => vector-dark_enable = 0
 if(!isset($_GET['is_on'])) return;
 
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Credentials: true');
+//header('Access-Control-Allow-Credentials: true');
 setcookie('vector-dark_origin', $_SERVER['HTTP_ORIGIN'], time() + 600);
 
 if($_GET['is_on'] == 'true') setcookie('vector-dark_enable', '1', 0, '/vector-dark');
