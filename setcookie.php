14ae8b870b4d2ef3b1967fd302e9a40a8e8e916c
m-szwarc <marcinszwarc@hotmail.com>
2019-07-29 18:03:32 +0200

    CORS

--- setcookie.php
+++ setcookie.php
@@ -3,13 +3,14 @@ if($_SERVER['REQUEST_METHOD'] == 'OPTIONS'){
     header('Access-Control-Allow-Origin: *');
     header('Access-Control-Allow-Methods: GET');
     header('Access-Control-Allow-Credentials: true');
+    return;
 }
 
 // GET: is_on=true => vector-dark_enable = 1 [DEFAULT]
 // GET: is_on=false => vector-dark_enable = 0
 if(!isset($_GET['is_on'])) return;
 
-header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Origin: '.$_SERVER['HTTP_ORIGIN']);
 //header('Access-Control-Allow-Credentials: true');
 setcookie('vector-dark_origin', $_SERVER['HTTP_ORIGIN'], time() + 600);
 
