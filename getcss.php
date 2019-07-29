5e06e70c870c9580d5a2f93a84df734fb6ce69a1
m-szwarc <marcinszwarc@hotmail.com>
2019-07-29 17:17:48 +0200

    getcss.php reads cookie

--- getcss.php
+++ getcss.php
@@ -1,4 +1,9 @@
 <?php
 header('Content-Type:text/css');
+
+if(isset($_COOKIE['vector-dark_enable'])){
+    if($_COOKIE['vector-dark_enable'] == '0') return;
+}
+
 echo(file_get_contents('vector.dark.css'));
 ?>
