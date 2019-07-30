3685ee0b9ea39842ad6c5a9dae9c0992ca5ed274
m-szwarc <marcinszwarc@hotmail.com>
2019-07-30 10:54:59 +0200

    outputs light-default version of css too

--- getcss.php
+++ getcss.php
@@ -2,7 +2,10 @@
 header('Content-Type:text/css');
 
 if(isset($_COOKIE['vector-dark_enable']) && !isset($_GET['force_css'])){
-    if($_COOKIE['vector-dark_enable'] == '0') return;
+    if($_COOKIE['vector-dark_enable'] == '0'){
+        echo(file_get_contents('vector.light.css'));
+        return;
+    }
 }
 
 echo(file_get_contents('vector.dark.css'));
