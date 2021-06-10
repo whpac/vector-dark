238e4a75a5d5bf0f2dce3c9023f2452d85d50f82
Marcin Szwarc <marcinszwarc@hotmail.com>
2021-06-10 19:30:00 +0200

    Przenoszę interfejs Window do głównego pliku

---
+++ source/ts/Mode.ts
@@ -0,0 +1,7 @@
+namespace Msz2001.VectorDark {
+    /** Opisuje dostępne tryby */
+    export enum Mode {
+        Light,
+        Dark
+    }
+}
--- source/ts/vector.dark.ts
+++ source/ts/vector.dark.ts
@@ -6,8 +6,17 @@ if(window.Msz2001_vectorDark_loaded === undefined) {
         window.Msz2001_vectorDark_pingujCookie = false;
     }
 
-    // Utwór kontroler po załadowaniu się strony
+    // Utwórz kontroler po załadowaniu się strony
     $(function () {
         new Msz2001.VectorDark.Controller();
     });
+}
+
+/** Rozszerza typ obiektu window o dodatkowe właściwości */
+interface Window {
+    /** Opisuje, czy już załadowano gadżet */
+    Msz2001_vectorDark_loaded: boolean | undefined;
+
+    /** Pozwala skonfigurować, czy pingować ToolForge po zmianie trybu */
+    Msz2001_vectorDark_pingujCookie: boolean | undefined;
 }
