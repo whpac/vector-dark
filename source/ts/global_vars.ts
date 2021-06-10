ceff595297ef406eb146ba0c369759ecaff7bf70
Marcin Szwarc <marcinszwarc@hotmail.com>
2021-06-10 19:17:34 +0200

    Aktualizuję moduł główny

--- source/ts/global_vars.ts
+++ source/ts/global_vars.ts
@@ -1,18 +1,6 @@
 /** Rozszerza typ obiektu window o dodatkowe właściwości */
 interface Window {
-
-    /** Kontener dla wszystkich potrzebnych obiektów */
-    Msz2001_vectorDark: {
-        /** Obrazek, odpowiadający za wykonywanie żądań */
-        obrazek_cors: HTMLImageElement | null,
-
-        /** Linki do przełączania trybu */
-        link_ciemny: HTMLElement | null,
-        link_jasny: HTMLElement | null,
-
-        /** Przechowuje informację, czy trwa inicjalizacja */
-        inicjalizacja: boolean;
-    };
+    Msz2001_vectorDark_loaded: boolean | undefined;
 
     /** Pozwala skonfigurować, czy pingować ToolForge po zmianie trybu */
     Msz2001_vectorDark_pingujCookie: boolean | undefined;
