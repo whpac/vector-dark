f516ac40f113610794407896c5f94cac53bf1dd9
Marcin Szwarc <marcinszwarc@hotmail.com>
2021-06-14 14:02:00 +0200

    Dodano przycisk do ustawień

--- source/ts/FloatingSwitcher.ts
+++ source/ts/FloatingSwitcher.ts
@@ -6,37 +6,48 @@ namespace Msz2001.VectorDark {
     export class FloatingSwitcher implements ThemeSwitcher {
         protected Controller: Controller;
         protected SwitcherWrapper: HTMLElement;
-        protected ToDarkModeLink: HTMLAnchorElement;
-        protected ToLightModeLink: HTMLAnchorElement;
+        protected ToDarkModeButton: HTMLButtonElement;
+        protected ToLightModeButton: HTMLButtonElement;
+        protected SettingsButton: HTMLButtonElement;
+        protected SettingsDialog: SettingsDialog;
 
         /**
          * Tworzy przełącznik trybów
          * @param controller Odwołanie do kontrolera
+         * @param settings_dialog Okno ustawień
          */
-        public constructor(controller: Controller) {
+        public constructor(controller: Controller, settings_dialog: SettingsDialog) {
             this.Controller = controller;
+            this.SettingsDialog = settings_dialog;
             this.SwitcherWrapper = document.createElement('div');
-            this.SwitcherWrapper.style.position = 'fixed';
-            this.SwitcherWrapper.style.bottom = '0';
-            this.SwitcherWrapper.style.left = '20px';
-            this.SwitcherWrapper.style.padding = '8px';
-            this.SwitcherWrapper.style.background = '#111';
+            this.SwitcherWrapper.classList.add('vector-dark-floating-switcher');
 
-            // Tworzy link do przełączenia na tryb ciemny
-            this.ToDarkModeLink = document.createElement('a');
-            this.ToDarkModeLink.href = 'javascript:void(0)';
-            this.ToDarkModeLink.innerText = '🌙';
-            this.ToDarkModeLink.style.display = 'none';
-            this.ToDarkModeLink.addEventListener('click', this.OnToDarkClick.bind(this));
-            this.SwitcherWrapper.appendChild(this.ToDarkModeLink);
+            // Tworzy przycisk do przełączenia na tryb ciemny
+            this.ToDarkModeButton = document.createElement('button');
+            this.ToDarkModeButton.type = 'button';
+            this.ToDarkModeButton.innerText = '🌙';
+            this.ToDarkModeButton.title = 'Przełącz na tryb ciemny';
+            this.ToDarkModeButton.style.display = 'none';
+            this.ToDarkModeButton.addEventListener('click', this.OnToDarkClick.bind(this));
+            this.SwitcherWrapper.appendChild(this.ToDarkModeButton);
 
-            // Tworzy link do przełączenia na tryb jasny
-            this.ToLightModeLink = document.createElement('a');
-            this.ToLightModeLink.href = 'javascript:void(0)';
-            this.ToLightModeLink.innerText = '☀';
-            this.ToLightModeLink.style.display = 'none';
-            this.ToLightModeLink.addEventListener('click', this.OnToLightClick.bind(this));
-            this.SwitcherWrapper.appendChild(this.ToLightModeLink);
+            // Tworzy przycisk do przełączenia na tryb jasny
+            this.ToLightModeButton = document.createElement('button');
+            this.ToLightModeButton.type = 'button';
+            this.ToLightModeButton.innerText = '🌞';
+            this.ToLightModeButton.title = 'Przełącz na tryb jasny';
+            this.ToLightModeButton.style.display = 'none';
+            this.ToLightModeButton.addEventListener('click', this.OnToLightClick.bind(this));
+            this.SwitcherWrapper.appendChild(this.ToLightModeButton);
+
+            // Tworzy przycisk ustawień
+            this.SettingsButton = document.createElement('button');
+            this.SettingsButton.classList.add('settings-button');
+            this.SettingsButton.type = 'button';
+            this.SettingsButton.innerText = '🔧';
+            this.SettingsButton.title = 'Ustawienia trybu ciemnego';
+            this.SettingsButton.addEventListener('click', this.OnSettingsClick.bind(this));
+            this.SwitcherWrapper.appendChild(this.SettingsButton);
 
             // Rejestruje nasłuchiwanie zmian trybu i umieszcza przełącznik w dokumencie
             this.Controller.SetNotificationOnModeChange(this.AdjustToCurrentMode.bind(this));
@@ -49,12 +60,12 @@ namespace Msz2001.VectorDark {
 
             switch(current_mode) {
                 case Mode.Light:
-                    this.ToLightModeLink.style.display = 'none';
-                    this.ToDarkModeLink.style.display = '';
+                    this.ToLightModeButton.style.display = 'none';
+                    this.ToDarkModeButton.style.display = '';
                     break;
                 case Mode.Dark:
-                    this.ToLightModeLink.style.display = '';
-                    this.ToDarkModeLink.style.display = 'none';
+                    this.ToLightModeButton.style.display = '';
+                    this.ToDarkModeButton.style.display = 'none';
                     break;
             }
         }
@@ -67,6 +78,11 @@ namespace Msz2001.VectorDark {
         /** Obsługuje kliknięcie na link "tryb ciemny" */
         protected OnToDarkClick() {
             this.Controller.RequestMode(Mode.Dark);
+        }
+
+        /** Obsługuje kliknięcie na przycisk ustawień */
+        protected OnSettingsClick() {
+            this.SettingsDialog.Open();
         }
 
         /** Dołącza przełącznik do dokumentu */
