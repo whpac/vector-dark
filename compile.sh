3604afe45b5dab1b18212ec7cd68e9ce277341fb
Marcin Szwarc <marcinszwarc@hotmail.com>
2023-02-28 13:37:06 +0100

    Akt. skryptu do kompilacji

--- compile.sh
+++ compile.sh
@@ -1,2 +1,8 @@
-../dart-sass/sass scss/dark:css/dark scss/light:css/light --no-source-map
+#!/bin/bash
+
+# First, compile the SCSS files to CSS
+SASS_PATH="../dart-sass/sass"
+$SASS_PATH scss/dark:css/dark scss/light:css/light --no-source-map
+
+# Then, compactify the CSS files
 python3 scripts/compactify.py css
